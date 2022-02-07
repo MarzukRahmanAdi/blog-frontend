79f5a0c2c909892c196b554668a498385182811f
IronSharkAdi <ironsharkadi2020@gmail.com>
2022-02-08 02:02:37 +0600

    dynamic card color setting feature added

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,11 @@
 
     
-    function Card() {
+    function Card({bg_color}) {
       return <div>
-          <div className=" w-full ">
-                <div className="card_body h-96  rounded-2xl ">
-                    <h1 className="font-bold text-6xl text-white ">How to get started with game dev</h1>
-                    <h4>By Adi</h4>
+          <div className={` w-11/12 mx-auto md:w-full  `}>
+                <div className={`card_body h-96 after:${bg_color}`}>    
+                    <h1 className="font-bold m-2 md:ml-5 md:mb-5 text-3xl lg:text-6xl text-white ">How to get started with game dev</h1>
+                    <h4 className="ml-2 md:ml-6 text-gray-50 z-10 md:mb-3">By Adi</h4>
                 </div>
           </div>
       </div>;
--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,8 +1,8 @@
 import Card from "./Card";
 
 function MainPage() {
-  return <div>
-    <Card/>
+  return <div className=" space-y-10">
+    <Card bg_color="bg-violet-500/30"/>
     <Card/>
 
   </div>;
